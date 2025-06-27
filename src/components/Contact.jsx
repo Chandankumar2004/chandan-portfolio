41ede080779967c3e4215125eba89481af92a027
chandan32005c <chandan32005c@gmail.com>
2025-06-27 10:06:56 +0000

    email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,7 @@ const Contact = () => {
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       formRef.current,
-      {
-        publicKey: import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
-      }
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     )
     .then(
       () => {
