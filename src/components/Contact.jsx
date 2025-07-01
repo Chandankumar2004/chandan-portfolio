2658706586be62f5dfa74ac8ac75d4f74ca99104
chandan32005c <chandan32005c@gmail.com>
2025-07-01 19:29:37 +0000

    ...

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,6 +31,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
+    console.log(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
